5ac7961f1dedc60f17b41a9e8b3d8aa746d99695
krishjp <krish4515.gk@gmail.com>
2023-10-14 10:30:28 +0000

    Create a counter in ReactJs

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+  onIncrease = () => {
+    this.setState(prevState => ({count: this.prevState.count + 1}))
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="counter-container">
+        <h1 className="heading">
+          The button has been clicked <span className="count">{count}</span>{' '}
+          times
+        </h1>
+        <p className="description">Click the button to increase the count!</p>
+        <button className="button" onClick={this.onIncrease}>
+          Click Me!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
